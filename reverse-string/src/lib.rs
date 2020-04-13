9af3a07b18b8ef83561b31080a57ade04e6f9f8a
iamareebjamal <jamal.areeb@gmail.com>
2020-04-14 00:07:35 +0530

    ref(reverse-string): Improve

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,9 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut string = String::new();
-
-    for c in input.chars().rev() {
-        string.push(c);
-    }
-
-    string
+    input.chars().rev().collect()
 }
