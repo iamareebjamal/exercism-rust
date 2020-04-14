b677ae51291fb124d4e51d596a94ff9533f99de4
iamareebjamal <jamal.areeb@gmail.com>
2020-04-14 19:31:43 +0530

    feat(matching-brackets): Implement

---
+++ matching-brackets/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut stack = Vec::new();
+
+    for c in string.chars() {
+        let valid = match c {
+            '{' | '[' | '(' => { stack.push(c); true },
+            '}' => stack.pop() == Some('{'),
+            ']' => stack.pop() == Some('['),
+            ')' => stack.pop() == Some('('),
+            _ => true
+        };
+
+        if !valid {
+            return false
+        }
+    }
+
+    stack.is_empty()
+}
